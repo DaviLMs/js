f7a04fe848d83a806c44d1af178b5188cd441602
DaviLMs <davilamarcateixeira@gmail.com>
2023-12-19 01:54:59 -0300

    js exempo 0.1
    js exemplo

---
+++ index.js
@@ -0,0 +1,18 @@
+function eventoClick(){
+    document.body.style.backgroundColor = "yellow";
+}
+function eventoClick(){
+    
+}
+function viraVermelho(){
+    let div = document.getElementById("teste");
+    div.style.backgroundColor = "red";
+}
+
+function viraAzul(){
+    let div = document.getElementById("teste");
+    div.style.backgroundColor = "blue";
+}
+function Limpatexto(){
+    document.getElementById("campoTexto").value = "";
+}
